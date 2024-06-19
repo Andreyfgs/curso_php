86a1eb4a499bd433d14c80000546dbebbd215b0e
Andreyfgs <fgsandrey@gmail.com>
2024-06-18 22:04:04 -0300

    Primeiro commit no github

---
+++ aula 1.php
@@ -0,0 +1,31 @@
+<?php
+/* Aula de número 1 dia 18-06-2024
+Turma BG0500
+Conteudos: echo, comentarios, variaveis
+Por: Andrey Fagundes
+
+*/
+
+// Este é um comentario do código, não será exibido em tela.
+echo "Olá mundo!<br>";
+
+# Outro tipo de comentário 
+/* Mais um tipo de comentário */
+
+$texto = "<br>Pequeno texto que você está lendo sobre o livro do Menino Maluquinho!";
+echo $texto;
+
+echo "<br>Outro texto:<br>Primeira linha<br>Segunda linha";
+
+$outravariavel = "<br>Outro grande texto!"; //Aqui é outro comentário
+echo $outravariavel;
+
+echo "<br>Calculos matemáticos<br>";
+echo 1+1;
+echo "<br>";
+echo 10-7;
+echo "<br>";
+echo 99*55;
+echo "<br";
+
+?>
