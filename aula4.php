ce5d023d985d76b0280df120b520cd38676916fa
Andreyfgs <fgsandrey@gmail.com>
2024-07-09 21:59:59 -0300

    Create aula4.php

---
+++ aula4.php
@@ -0,0 +1,11 @@
+<?php
+/*Utilizar o laço de repetição FOR para criar a tabuada do numero informado pelo usuario.
+Ex.: usuario informou o numero 4, fazer a tabuado do 4, usuario informou o 9, fazer a tabuada d0 9 e assim por diante.*/
+
+$numero = 9; // numero que usuario informou
+$resultado = "";
+
+for ($i = 1; $i <=10 ; $i++ ) 
+  { 
+    echo $resultado = $numero * $i . "<br>";
+  }
