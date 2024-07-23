64b1637a64346cce4e61b8a0fee6854865d93b95
Andreyfgs <fgsandrey@gmail.com>
2024-07-23 20:20:34 -0300

    tema de casa

--- tema.php
+++ tema.php
@@ -9,31 +9,56 @@
     Soma Impares = 545455424
 */
 
+// Usando o laço While
+
+// Soma dos PARES
 $i = 1;
 $soma = 0;
+    
     while ($i <= 100){ 
-       $resto = $i % 2;  
+    $resto = $i % 2;  //$resto vai armazenar o resultado da divisão $ por 2
     if ($resto == 0){  //se for 0 é par 
     $soma = $soma + $i;
     } $i++;
-} echo "A soma de todos os números pares é ". $soma;
-echo "<hr>";
-
-$i = 1;
-
-$resultado = 0; 
-
-echo $i % 2;
+} echo "A soma de todos os números pares é = ". $soma;
 
 echo "<hr>";
-
+// Soma dos ímpares
 $i = 1;
 $resultado = 0;
 
-while ($i <= 100){
-    $resto = $i % 2;
+    while ($i <= 100){
+    $resto = $i % 2; //$resto vai armazenar o resultado da divisão $i por 2
     if ($resto == 1){ //se sobrar 1 é ímpar
-     $resultado = $resultado + $i;
+    $resultado = $resultado + $i;
     } $i++;
+} echo " A soma de todos os números ímpares é = ". $resultado;
 
-} echo " A soma de todos os números ímpares é ". $resultado;
+echo "<hr>";
+// Usando o laço FOR
+$somaDosPares = 0;
+$somaDosImpares = 0;
+
+    for($i = 1; $i <= 100; $i++){
+    $imparoupar = $i % 2;
+    if($imparoupar == 0){
+    $somaDosPares = $somaDosPares + $i; 
+    }else{
+    $somaDosImpares += $i;
+    }
+} 
+echo "A soma do par é $somaDosPares<br>";   
+echo "A soma dos Impares é $somaDosImpares";
+
+//Outra forma
+/* 
+$somadospares = 0;
+$somadosimpares = 0;
+    
+    While($i <= 100){
+    if($i % 2 ==0){
+    $somadospares += $i;
+    }else {
+    $somadosimpares += $i;
+    }*/
+
