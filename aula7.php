e45a7763a66c4b2f4e702517fecbf8282ff081b6
Andreyfgs <fgsandrey@gmail.com>
2024-07-30 21:58:10 -0300

    Tema de casa

---
+++ aula7.php
@@ -0,0 +1,59 @@
+<?php
+
+$vetor = array();
+//$tamanho = count($vetor); descobrir o tamanho do vetor.
+$somaDosPares = 0;
+$somaDosImpares = 0;
+$vetorPar = [];
+$vetorImpar = [];
+
+for ($i =1; $i < 100; $i++) {
+    if ($i % 2 == 0) {
+        
+        $somaDosPares += $i;
+        // $vetorPar = array_push($vetorPar, $i);
+        $vetorPar[] = $i;
+        
+        
+    }else {
+        
+        $somaDosImpares += $i;
+        //$vetorImpar = array_push($vetorImpar, $i);
+        $vetorImpar[] = $i; // $VetorImpar está adicionando o $i no array.
+    } 
+    
+}  
+//$tamanhoPar = count($tamanhoPar); da para trocar a condição por essa fica mais claro.
+
+for($p = 0; $p < count($vetorPar); $p++) {
+
+    echo $vetorPar[$p] . ","."<br>";
+
+}
+
+for($p = 0; $p < count($vetorImpar); $p++) {
+echo "<br>";
+    echo $vetorImpar[$p] . ",";
+
+}
+echo "<hr>";
+echo "LAÇO WHILE";
+echo "<br>";
+echo "PAR". "<br>";
+
+$i = 0;
+while($i < count($vetorPar)) {
+
+    echo $vetorPar[$i]. ",";
+    $i++;
+} 
+
+echo "<br>";
+echo "IMPAR" . "<br>";
+
+$t = 0;
+while($t < count($vetorImpar)) {
+
+    echo $vetorImpar[$t] . ",";
+    $t++;
+}
--- tema.php
+++ tema.php
@@ -1,83 +1,36 @@
-<?php 
-/*
-    Somar todos os números Pares de 1-100 
-    Somar todos os números IMPARES de 1-100
-    Utilizando o laço de repetiçao  While 
-    apos, mostrar o resultado da soma desses números;
-    ex..:
-    Soma Pares = 123211123
-    Soma Impares = 545455424
-*/
+<?php
 
-// Usando o laço While
+//alterar a ordem para descrescente usando outro FOR.
+//PELO MENOS 3 VARIAVEIS (AREA DE TRANSFERENCIA, DESTINO )
 
-// Soma dos PARES
-/*echo "<strong>Exercício usando o laço WHILE</strong>";
-echo "<br>";
-$i = 1;
-$soma = 0;
-    
-    while ($i <= 100){ 
-    $resto = $i % 2;  //$resto vai armazenar o resultado da divisão $ por 2
-    if ($resto == 0){  //se for 0 é par 
-    $soma = $soma + $i;
-    } $i++;
-} echo "A soma de todos os números pares é = ". $soma;
-
-echo "<br>";
-// Soma dos ímpares
-$i = 1;
-$resultado = 0;
-
-    while ($i <= 100){
-    $resto = $i % 2; //$resto vai armazenar o resultado da divisão $i por 2
-    if ($resto == 1){ //se sobrar 1 é ímpar
-    $resultado = $resultado + $i;
-    } $i++;
-} echo " A soma de todos os números ímpares é = ". $resultado."<br>";
-
-echo "<br>";
-
-// Usando o laço FOR
-echo "<strong>Exercício usando o laço FOR</strong>";
-echo "<br>";
-
+$vetor = array();
+//$tamanho = count($vetor); descobrir o tamanho do vetor.
 $somaDosPares = 0;
 $somaDosImpares = 0;
+$vetorPar = [];
+$vetorImpar = [];
 
-    for($i = 1; $i <= 100; $i++){
-    $imparoupar = $i % 2;
-    if($imparoupar == 0){
-    $somaDosPares = $somaDosPares + $i; 
-    }else{
-    $somaDosImpares += $i;
-    }
-} 
-echo "A soma do par é $somaDosPares<br>";   
-echo "A soma dos Impares é $somaDosImpares<br>";
-echo "<br>";
-//Outra forma
-/* 
-$somadospares = 0;
-$somadosimpares = 0;
-    
-    While($i <= 100){
-    if($i % 2 ==0){
-    $somadospares += $i;
+for ($i =1; $i < 100; $i++) {
+    if ($i % 2 == 0) {
+        
+        $somaDosPares += $i;
+        // $vetorPar = array_push($vetorPar, $i);
+        $vetorPar[] = $i;
+        
+        
     }else {
-    $somadosimpares += $i;
-    }*/
-
-    echo "<strong>Tabuada legal</strong>";
+        
+        $somaDosImpares += $i;
+        //$vetorImpar = array_push($vetorImpar, $i);
+        $vetorImpar[] = $i; // $VetorImpar está adicionando os valores do $i no array.
+    } 
     
-    for($tabuada=1; $tabuada <= 10; $tabuada++) {
-        echo "<br> Tabuada do $tabuada <br>";
-     // Inicia o laço interno para multiplicar a tabuada por 0-10   
-    for($n=0; $n <=10; $n++){
-        echo "$tabuada x $n = " . $tabuada*$n . "<br>";
-        }
-    
-    }  
+}  //criar outros 2 for para inverter a ordem.
+$t= 0
+for($t = 0; $t <= count($vetorPar); $t++) {
+    echo $vetorPar[$t];
+}
+ 
+for($p = 0; $p < count($vetorImpar); $p++) {}
 
-    
-
+//METODOS DE ORDENAR UM ARRAY EX:BUBLLE SORT. nome MATUSSTAFURA/BUBBLE
