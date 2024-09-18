88da6a65b3e90be99bd65d1fe7f864897041b2b5
Andreyfgs <fgsandrey@gmail.com>
2024-09-17 21:59:16 -0300

    mariadb

--- aula14.php
+++ aula14.php
@@ -1,7 +1,5 @@
 <?php
 
-<?php
-
 /**
  * Criar uma classe/objeto que representa um banco 
  * esta classe devera ter as operações basicas, como:
@@ -16,37 +14,39 @@
 
  class Banco{
 
-    public $saldo = 2000;
-    public $valor = 1000;
+    public $saldo = 1000;
     
     public function saque($valor){
 
-    if($valor <=0 || $valor == ""){ 
-        return "<br>Não é possivel efetuar o saque, informe um valor. <br>";
-    }
+        $valor =  preg_replace("/[^0-9]/", "", $valor);
 
-    if( $valor > $saldo){
-        return "<br>Não é possivel efetuar o saque, o seu saldo é: $valor o valor sacado é: $saldo <br>";
-    }
+        if($valor <=0 || $valor == ""){ 
+            return "<br>Não é possivel efetuar o saque, informe um valor. <br>";
+        }
+
+        if( $valor > $this->saldo){
+            return "<br>Não é possivel efetuar o saque, o seu saldo é: $this->saldo o valor de saque : $valor <br>";
+        }
 
        $this->saldo -= $valor;
     }
    
-   public function deposito($valor){
-    
-    $valor =  preg_replace("/[^0-9]/", "", $valor);
-    
-    if($valor <=0 || $valor == ""){ // entre () sempre ira executar primeiro tudo que está dentro de ().
-        return "<br>O valor do deposito deve ser positivo<br> ";    
-    }
+    public function deposito($valor){
+
+        $valor =  preg_replace("/[^0-9]/", "", $valor);
+
+        if($valor <=0 || $valor == ""){ // entre () sempre ira executar primeiro tudo que está dentro de ().
+            return "<br>O valor do deposito deve ser positivo<br> ";    
+        }
+
         $this->saldo += $valor;
     }   
 
    public function extrato(){
-        return $this->saldo . $saldo;
+        return $this->saldo . "<br>";
     }
 
-    public function tranferencia($valor, $destinatario){
+    public function transferencia($valor, $destinatario){
 
         $valor =  preg_replace("/[^0-9]/", "", $valor);
 
@@ -55,7 +55,7 @@
         }
 
         if($valor > $this->saldo){
-            return "Não foi possivel realizar a transferência. Por favor, insira um valor válido.";
+            return "Não foi possivel realizar a transferência.  Valor insirido: $valor saldo disponivel :" . $this->saldo . "<br>";
         }
 
         $this->saldo -= $valor;
@@ -64,17 +64,20 @@
     }
 
     private function transfCompleta($destinatario) {
-        return "Transferência concluída";
+        return "Transferência concluída, destinatário:" . $destinatario . "<br>";
     }
  }
 
- $banco = new Banco();
- echo $banco->saque(20);
+ $banco = new Banco(); //variavel $banco recebe a class Banco()
+ echo $banco->saque(100);
  echo "Saldo após saque: " . $banco->extrato() . "<br>";
  
- echo $banco->deposito(20);
+ echo $banco->deposito(10);
  echo "Saldo após depósito: " . $banco->extrato() . "<br>";
  
- echo $banco->transferencia(200);
+ echo $banco->transferencia(200, "jao");
  echo "Saldo após transferência: " . $banco->extrato() . "<br>";
+
+ echo $banco->extrato();
+ echo "Extrato: ". $banco->extrato();
  
---
+++ aula15.php
@@ -0,0 +1,32 @@
+<?php
+
+//CRUD
+/*
+    Create
+    Read
+    Update
+    Delete
+*/
+
+/**
+* S - Structure 
+* Q - Query                 
+* L - lenguage
+*/
+
+                         // Pesquisar na internet
+// Banco relacional e não relacional 
+
+ class Usuario{
+    public $cpf;
+    public $email;
+    public $senha;
+    public $permissão;
+
+    public function criar() {}
+    public function getUsuario($login) {}
+    public function getUsuarios() {}
+    public function atualizar($id) {}
+    public function excluir($id) {}
+}
+
