aad99272fad5269d56913d0ff4373999e753b84b
Andreyfgs <fgsandrey@gmail.com>
2024-09-17 22:00:40 -0300

    Add files via upload

---
+++ dump-curso_24-202409172154.sql
@@ -0,0 +1,9 @@
+LOCK TABLES `Usuario` WRITE;
+/*!40000 ALTER TABLE `Usuario` DISABLE KEYS */;
+INSERT INTO `Usuario` VALUES (1,'05655202026','fgsandrey@gmail.com','1234'),(2,'05655202028','ariel.felippi@gmail.com','1234'),(3,'05655202028','ariel.felippi@gmail.com','1234');
+/*!40000 ALTER TABLE `Usuario` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Dumping routines for database 'curso_24'
+--
